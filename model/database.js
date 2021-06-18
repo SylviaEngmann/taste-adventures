6538ff8a4c3514d84efe0ea98ea30e5d0fade22d
bishop027 <bishopmichelle@live.com>
2021-06-18 22:05:35 +0200

    added more routes

--- model/database.js
+++ model/database.js
@@ -7,12 +7,11 @@ const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
-//change your personal password or username here
 const con = mysql.createConnection({
-  host: DB_HOST || DB_HOST,
-  user: DB_USER || DB_USER,
-  password: DB_PASS || DB_PASS,
-  database: DB_NAME || DB_NAME,
+  host: DB_HOST || "127.0.0.1",
+  user: DB_USER || "root",
+  password: DB_PASS || "root",
+  database: DB_NAME || "tasteadventure",
   multipleStatements: true
 });
 
